ec21c4b729a310cc6610f445a9d9b033639a06b9
R. Romain <romain.re@gmail.com>
2022-09-21 00:09:00 -0400

    add tests with scenarios for formatBytes function

---
+++ tests/index.scenarios.ts
@@ -0,0 +1,191 @@
+export default [
+  {
+    input: {
+      bytes: 0,
+      decimals: undefined
+    },
+    output: "0 Byte"
+  },
+  {
+    input: {
+      bytes: 1024,
+      decimals: undefined
+    },
+    output: "1 KB"
+  },
+  {
+    input: {
+      bytes: 1024000,
+      decimals: undefined
+    },
+    output: "1 MB"
+  },
+  {
+    input: {
+      bytes: 1024000000,
+      decimals: undefined
+    },
+    output: "1 GB"
+  },
+  {
+    input: {
+      bytes: 1024000000000,
+      decimals: undefined
+    },
+    output: "1 TB"
+  },
+  {
+    input: {
+      bytes: 1024000000000000,
+      decimals: undefined
+    },
+    output: "1 PB"
+  },
+  {
+    input: {
+      bytes: 1024000000000000000,
+      decimals: undefined
+    },
+    output: "1 EB"
+  },
+  {
+    input: {
+      bytes: 1.024e+21,
+      decimals: undefined
+    },
+    output: "1 ZB"
+  },
+  {
+    input: {
+      bytes: 1.024e+24,
+      decimals: undefined
+    },
+    output: "1 YB"
+  },
+  {
+    input: {
+      bytes: 0,
+      decimals: 1
+    },
+    output: "0 Byte"
+  },
+  {
+    input: {
+      bytes: 1024,
+      decimals: 1
+    },
+    output: "1.0 KB"
+  },
+  {
+    input: {
+      bytes: 1024000,
+      decimals: 1
+    },
+    output: "1.0 MB"
+  },
+  {
+    input: {
+      bytes: 1024000000,
+      decimals: 1
+    },
+    output: "1.0 GB"
+  },
+  {
+    input: {
+      bytes: 1024000000000,
+      decimals: 1
+    },
+    output: "1.0 TB"
+  },
+  {
+    input: {
+      bytes: 1024000000000000,
+      decimals: 1
+    },
+    output: "1.0 PB"
+  },
+  {
+    input: {
+      bytes: 1024000000000000000,
+      decimals: 1
+    },
+    output: "1.0 EB"
+  },
+  {
+    input: {
+      bytes: 1.024e+21,
+      decimals: 1
+    },
+    output: "1.0 ZB"
+  },
+  {
+    input: {
+      bytes: 1.024e+24,
+      decimals: 1
+    },
+    output: "1.0 YB"
+  },
+  {
+    input: {
+      bytes: 0,
+      decimals: 2
+    },
+    output: "0 Byte"
+  },
+  {
+    input: {
+      bytes: 1024,
+      decimals: 2
+    },
+    output: "1.02 KB"
+  },
+  {
+    input: {
+      bytes: 1024000,
+      decimals: 2
+    },
+    output: "1.02 MB"
+  },
+  {
+    input: {
+      bytes: 1024000000,
+      decimals: 2
+    },
+    output: "1.02 GB"
+  },
+  {
+    input: {
+      bytes: 1024000000000,
+      decimals: 2
+    },
+    output: "1.02 TB"
+  },
+  {
+    input: {
+      bytes: 1024000000000000,
+      decimals: 2
+    },
+    output: "1.02 PB"
+  },
+  {
+    input: {
+      bytes: 1024000000000000000,
+      decimals: 2
+    },
+    output: "1.02 EB"
+  },
+  {
+    input: {
+      bytes: 1.024e+21,
+      decimals: 2
+    },
+    output: "1.02 ZB"
+  },
+  {
+    input: {
+      bytes: 1.024e+24,
+      decimals: 2
+    },
+    output: "1.02 YB"
+  }
+];
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -4,6 +4,8 @@ import { assert } from 'chai';
 import { formatBytes } from '../src/index';
 import npmPackage from '../src/index';
 
+import SCENARIOS from './index.scenarios'
+
 describe('Formater', () => {
   it('should be an object', () => {
     assert.isObject(npmPackage);
@@ -18,10 +20,13 @@ describe('formatBytes Function', () => {
   it('should be a function', () => {
     assert.isFunction(formatBytes);
   });
+  for (let index = 0; index < SCENARIOS.length; index++) {
+    const scenario = SCENARIOS[index];
+    it(`should return "${scenario.output}" with "${scenario.input.bytes}" bytes and "${scenario.input.decimals}" decimals`, () => {
+      const expected = scenario.output;
+      const actual = formatBytes(scenario.input.bytes, scenario.input.decimals);
+      assert.equal(actual, expected);
+    });
 
-  it('should return "0 Byte" with bytes parameter equal to 0', () => {
-    const expected = '0 Byte';
-    const actual = formatBytes(0);
-    assert.equal(actual, expected);
-  });
+  }
 });
